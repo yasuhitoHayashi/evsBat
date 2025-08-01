777710bdb7e0c62fdb14a5f8765979ca830e79c2
dangom_ya <59243447+yasuhitoHayashi@users.noreply.github.com>
2025-08-01 10:11:40 +0900

    update comment
    Signed-off-by: dangom_ya <59243447+yasuhitoHayashi@users.noreply.github.com>

--- process_fft.py
+++ process_fft.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-# 定数の設定
-FS = 1000  # サンプリング周波数 (Hz)
-WINDOW_LENGTH = 4096  # FFTの窓長
+# Set constants
+FS = 1000  # Sampling frequency (Hz)
+WINDOW_LENGTH = 4096  # FFT window length
 
 
 def preprocess_time_series(time_series):
-    """時系列データを前処理する: 直流成分を削除し、Hanning窓を適用する"""
-    time_series = time_series - np.mean(time_series)  # トレンドの除去
+    """Preprocess the time series: remove DC component and apply Hanning window"""
+    time_series = time_series - np.mean(time_series)  # Remove trend
     windowed_series = time_series * np.hanning(
         len(time_series)
-    )  # Hanning窓の適用
+    )  # Apply Hanning window
     return windowed_series
 
 
 def pad_time_series(time_series, target_length=WINDOW_LENGTH):
-    """データの長さが窓長より短い場合、後ろに0詰めを行う"""
+    """If the data length is shorter than the window length, pad with zeros at the end"""
     if len(time_series) < target_length:
         return np.pad(
             time_series, (0, target_length - len(time_series)), "constant"
@@ -25,7 +25,7 @@ def pad_time_series(time_series, target_length=WINDOW_LENGTH):
 
 
 def compute_fft(time_series, win_len=WINDOW_LENGTH, fs=FS):
-    """FFTを実行し、周波数とその振幅を取得する"""
+    """Perform FFT and obtain frequency and its amplitude"""
     fft_result = np.fft.fft(time_series)
     freqs = np.fft.fftfreq(win_len, d=1 / fs)
     return freqs[: win_len // 2], 20 * np.log(
